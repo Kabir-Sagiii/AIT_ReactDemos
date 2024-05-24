029ef93de9a5922f8e864e9ce3c4deb14fce2a87
Kabir <sagithedeveloper@gmail.com>
2024-05-24 08:08:29 +0530

    Added the code

--- S11_16-04-2024/MSection2.js
+++
@@ -1,8 +0,0 @@
-function ImageBox(props) {
-  return (
-    <div className="imagebox">
-      <img src={props.imageurl} width="170" height="170" />
-      <h3>{props.title}</h3>
-    </div>
-  );
-}
