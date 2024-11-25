9514924572bc0d86d59e64f7cf5cb9da805e25a7
Kabir <sagithedeveloper@gmail.com>
2024-11-25 08:01:37 +0530

    Added the code

---
+++ S10/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h2>App Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+        architecto temporibus perspiciatis nobis, officiis voluptatibus
+        explicabo quos amet, alias corporis, commodi inventore saepe a illo
+        neque cum nisi tempora. Ipsa pariatur nam aperiam illum assumenda
+        asperiores, optio libero ab minima suscipit. Eaque dolor nostrum porro
+        est, assumenda aliquam necessitatibus incidunt. Facere, laborum ipsam
+        iure illo quo quasi magnam doloremque rem modi deserunt reprehenderit
+        dolorem magni excepturi temporibus vero rerum voluptas repellendus
+        praesentium eligendi totam? Quae, est reprehenderit hic delectus earum
+        possimus neque ratione quidem qui perspiciatis culpa dolore incidunt
+        optio consequatur harum esse distinctio aliquid obcaecati saepe unde
+        assumenda. Explicabo?
+      </p>
+      <button>Explore</button>
+    </div>
+  );
+}
+
+export default App;
